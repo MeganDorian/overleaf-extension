d08da5f41d87bec89a3136a9146ae20a4eddd470
Ilia <istudyatuni@gmail.com>
2022-11-08 23:50:28 +0300

    add bulma

--- ui/src/Popup.svelte
+++ ui/src/Popup.svelte
@@ -1,3 +1,13 @@
+<script>
+  import Input from "./lib/Input.svelte"
+</script>
+
 <main>
-  <button on:click={() => open('settings.html')}>Open settings</button>
+  <div>
+    <button class="button">Log out</button>
+    <h3 class="title is-3">Посылатор</h3>
+    <button class="button" on:click={() => open('settings.html')}>Settings</button>
+  </div>
+
+  <Input input_value="" label_name="asdf" />
 </main>
