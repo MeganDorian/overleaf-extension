78a71379293d4b057d0e22a6237891fea8d9b3b8
Ilia <43654815+istudyatuni@users.noreply.github.com>
2022-11-08 21:38:22 +0300

    feat: support 2 entry points - popup and settings (#3)

---
+++ ui/src/Popup.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Counter from "./lib/Counter.svelte";
+
+    let initial = 5
+
+    let h = '<h2>sample @html</h2>'
+</script>
+
+<main>
+  <button on:click={() => open('settings.html')}>Open settings</button>
+
+  <p>
+    <Counter {initial} />
+  </p>
+  <Counter />
+
+  <p>{@html h}</p>
+</main>
+
+<style>
+  p > :global(button) {
+    color: red;
+  }
+</style>
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/ui-dist/'
+  base: '/ui/dist/',
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html'),
+        settings: resolve(__dirname, 'settings.html')
+      }
+    }
+  }
 })
