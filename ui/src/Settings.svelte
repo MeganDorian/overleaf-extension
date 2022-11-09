6869de19791c3d21e260f741321d01daeec19c99
MeganDorian <47370044+MeganDorian@users.noreply.github.com>
2022-11-09 11:03:09 +0300

    Settings (#9)
    * feat: added base settings component

--- ui/src/Settings.svelte
+++ ui/src/Settings.svelte
@@ -1,3 +1,29 @@
-<main>
-  <h1>Settings</h1>
-</main>
+<script>
+    import Menu from "./lib/Menu.svelte";
+</script>
+
+<div class="logo-container">
+    <div class="logo"></div>
+</div>
+
+<div class="div-container">
+    <h1 class="font">Настройки</h1>
+    <Menu/>
+</div>
+
+<style>
+    .logo-container {
+        height: 43px;
+        width: 120px;
+        position: absolute;
+        top: 40px;
+        left: 150px
+    }
+
+    .logo {
+        background-image: url("assets/full_logo.svg");
+        background-size: contain;
+        background-position: left center;
+        background-repeat: no-repeat;
+    }
+</style>
---
+++ ui/src/lib/Menu.svelte
@@ -0,0 +1,30 @@
+<div class="sidebar">
+    <aside class="menu">
+        <p class="menu-label">Предметы</p>
+        <ul class="menu-list">
+            <li><a class="is-active">Алгоритмы</a></li>
+            <li><a>Дискретная математика</a></li>
+            <li><a>Функциональное программирование</a></li>
+            <li><a>Английский язык</a></li>
+            <li><a>Unix</a></li>
+            <li><a>Машинное обучение</a></li>
+            <li><a>Базы данных</a></li>
+            <li><a>C++</a></li>
+        </ul>
+        <p class="menu-label">Пользователь</p>
+        <ul class="menu-list">
+            <li><a>Логин</a></li>
+            <li><a>ФИО</a></li>
+        </ul>
+    </aside>
+</div>
+
+<style>
+    .sidebar {
+        width: 190px;
+    }
+
+    .menu-list a.is-active {
+        background-color: #4caf50;
+    }
+</style>
