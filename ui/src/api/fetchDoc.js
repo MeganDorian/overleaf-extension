1417926d551cf21836b039c9d683eeaf494a7ae2
MeganDorian <47370044+MeganDorian@users.noreply.github.com>
2022-11-09 14:21:01 +0300

    Fetch doc (#15)
    * Function that fetches pdf as blob and resends it to native app.
    
    * Get link to PDF from overleaf
    
    * Whole starts when requestDoc() is called
    * Ends (now - fails) sending data to localhost
    
    * Get link to PDF from overleaf
    
    * Whole starts when requestDoc() is called
    * Ends (now - fails) sending data to localhost
    
    * Function that fetches pdf as blob and resends it to native app.
    
    * Get link to PDF from overleaf
    
    * Whole starts when requestDoc() is called
    * Ends (now - fails) sending data to localhost
    
    * Get link to PDF from overleaf
    
    * Whole starts when requestDoc() is called
    * Ends (now - fails) sending data to localhost
    
    Co-authored-by: Artemiy Lazarev <artemiy.lazarevx7@gmail.com>

---
+++ ui/src/api/fetchDoc.js
@@ -0,0 +1,36 @@
+
+// Returns what localhost returns
+export async function fetchDoc(docUrl, nativeUrl) {
+    let content = await fetch(docUrl)
+        .then(response => response.blob())
+
+    console.log(`Fetched ${content.size} bytes...`);
+
+    return fetch(
+        nativeUrl,
+        {
+            method: "POST",
+            body: content
+        }
+    )
+        .then(res => res.json())
+        .then(json => json.code)
+        .catch(console.error)
+}
+
+export async function requestDoc() {
+    console.log("Doc requested... Cookies:");
+    console.log(chrome.cookies);
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, "My message");
+    });
+
+}
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+    console.log(`Received message: ${JSON.stringify(message)}`);
+    if (message.ok) {
+        fetchDoc(message.url, "http://localhost");
+    }
+})
