5fb0004fddc533bc2e8d7192bc859dc0296d6a09
Anelya Dusaeva <an.dusaeva+git@gmail.com>
2022-11-10 01:55:02 +0300

    added new data structure and subject representation at the setting page

---
+++ ui/src/lib/components/MsgView.svelte
@@ -0,0 +1,45 @@
+<script>
+    import {c_all_data} from "src/templates" 
+    export let subject_id;
+    let id = 0;
+</script>
+
+<div class="block">
+
+    {#if subject_id === 'algo'}   
+        id = 0
+    {:else if subject_id === "diskr"}
+        id = 1
+    {/if}
+    
+    {#each c_all_data.subjects[id].msg_view as item}
+        <h4 class="title is-4">
+            {item.surname}        
+            <button class="delete"></button>
+        </h4>
+        <label class="label">Email</label>
+        <p class="notification">
+            {item.email}
+        </p>
+        <!-- <div class="control">
+          <input class="input" type="email" value={item.email} disabled>
+        </div> -->
+        
+        <label class="label">Тема</label>
+        <p class="notification">
+            {item.topic}
+        </p>
+        <!-- <div class="control">
+          <input class="input" type="email" value={item.topic} disabled>
+        </div> -->
+
+        <label class="label">Текст сообщения</label>
+        <p class="notification">
+            {c_all_data.msg_body}
+        </p>
+
+    {/each}
+
+
+</div>
+
