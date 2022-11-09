015d005ca287616d94521404634f62ae3ed26d77
Dmitry Karagodin <32226272+DKARAGODIN@users.noreply.github.com>
2022-11-09 20:06:18 +0300

    Cors for file (#23)

--- java_email_sender/src/main/java/ru/mse/overleafextension/boundary/SendEmailHandler.java
+++ java_email_sender/src/main/java/ru/mse/overleafextension/boundary/SendEmailHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
 import ru.mse.overleafextension.control.SmtpSenderService;
 import ru.mse.overleafextension.entity.OverleafMessage;
 
@@ -28,6 +29,7 @@ public class SendEmailHandler implements HttpHandler {
             exchange.setStatusCode(400);
             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
             exchange.getResponseSender().send("{\"errorDescription\":\""+e.getMessage() +"\"}");
+            exchange.getResponseHeaders().put(new HttpString("Access-Control-Allow-Origin"), "*");
         }
     }
 }
