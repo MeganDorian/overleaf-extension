0b2ba70fa8874af007f3d49496d0a6cda869a45a
Julie Kosenko <mishakollins68@gmail.com>
2022-11-10 18:38:01 +0300

    refactor: changed using of default select to project Select

--- ui/src/lib/components/Login.svelte
+++ ui/src/lib/components/Login.svelte
@@ -1,30 +1,38 @@
 <script context="module">
-	import Input from 'src/lib/components/Input.svelte'
+    import Input from 'src/lib/components/Input.svelte'
+    import Select from "src/lib/components/Select.svelte"
 </script>
 
 <script>
 	export let email = ''
 	export let password = ''
 	export let smtpService = ''
+
+    let providers = [
+        {
+            key: 'gmail',
+            name: 'Gmail'
+        },
+        {
+            key: 'yandex',
+            name: 'Yandex'
+        }
+    ]
 </script>
 
-<content>
-	<h5 class="title is-5">Войти в аккаунт</h5>
-	<div class="notification is-info content">
-		Для входа в аккаунт введите почту и пароль приложения почты (НЕ АККАУНТА), который можно получить по следующим инструкциям:
-		<ul>
-			<li><a href="https://support.google.com/accounts/answer/185833?hl=ru">Gmail</a></li>
-			<li><a href="https://yandex.ru/support/id/authorization/app-passwords.html">Яндекс</a></li>
-		</ul>
-	</div>
-	<div class="block">
-		<label for="smtpService" class="has-text-weight-bold">Сервис</label>
-		<br>
-		<select name="smtpService" id="smtpService" bind:value={smtpService}>
-			<option value="gmail">Gmail</option>
-			<option value="yandex">Yandex</option>
-		</select>
-	</div>
-	<Input id="login-email" label="Ваша почта (логин)" type="email" bind:value={email} />
-	<Input id="login-email" label="Пароль приложения" type="password" bind:value={password} />
-</content>
+<main>
+    <h5 class="title is-5">Войти в аккаунт</h5>
+    <div class="notification is-info content" style="background-color: #4caf50">
+        Для входа в аккаунт введите почту и пароль приложения почты (НЕ АККАУНТА), который можно получить по следующим
+        инструкциям:
+        <ul>
+            <li><a href="https://support.google.com/accounts/answer/185833?hl=ru">Gmail</a></li>
+            <li><a href="https://yandex.ru/support/id/authorization/app-passwords.html">Яндекс</a></li>
+        </ul>
+    </div>
+    <div class="block">
+        <Select options={providers} id="smtpService" title="Сервис почты" bind:value={smtpService}/>
+    </div>
+    <Input id="login-email" label="Ваша почта (логин)" type="email" bind:value={email}/>
+    <Input id="login-email" label="Пароль приложения" type="password" bind:value={password}/>
+</main>
