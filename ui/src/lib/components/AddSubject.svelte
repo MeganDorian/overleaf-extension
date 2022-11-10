aa9a145955afb4cca57e6c81407327c5551881c8
Ilia <istudyatuni@gmail.com>
2022-11-10 20:16:58 +0300

    feat: add subject

--- ui/src/lib/components/AddSubject.svelte
+++ ui/src/lib/components/AddSubject.svelte
@@ -1,16 +1,47 @@
+<script context="module">
+    import Input from "src/lib/components/Input.svelte"
+    import Button from "src/lib/components/Button.svelte"
+
+    import { subjectsStore } from 'src/lib/store/subjects'
+</script>
+
 <script>
-    import Input from "src/lib/components/Input.svelte";
+    let subject_name = '',
+        teacher_name = '',
+        teacher_email = '',
+        topic = ''
+
+    $: add_button_disabled =
+        subject_name === ''
+        || teacher_name === ''
+        || teacher_email === ''
+        || topic === ''
+
+    function add_subject() {
+        let msg_view = {
+            surname: teacher_name,
+            email: teacher_email,
+            topic,
+        }
+        subjectsStore.add_subject(subject_name, msg_view)
+
+        subject_name = ''
+        teacher_name = ''
+        teacher_email = ''
+        topic = ''
+    }
 </script>
 
 <menu>
     <div class="block">
-        <Input id="subject_name" label="Название предмета" type="text"></Input>
+        <Input id="subject_name" label="Название предмета" type="text" bind:value={subject_name} />
         <br>
-        <Input id="teacher_name" label="Имя преподавателя" type="text"></Input>
+        <Input id="teacher_name" label="Имя преподавателя" type="text" bind:value={teacher_name} />
         <br>
-        <Input id="teacher_email" label="Почта преподавателя" type="email"></Input>
+        <Input id="teacher_email" label="Почта преподавателя" type="email" bind:value={teacher_email} />
         <br>
-        <Input id="topic" label="Тема письма" type="text"></Input>
+        <Input id="topic" label="Тема письма" type="text" bind:value={topic} />
         <br>
+        <Button content="Добавить" on:click={add_subject} disabled={add_button_disabled} class="mt-2" />
     </div>
 </menu>
--- ui/src/lib/store/subjects.js
+++ ui/src/lib/store/subjects.js
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid'
+
 import { writable } from 'svelte/store'
 
 import { c_all_data } from 'src/templates'
@@ -6,32 +8,50 @@ const storage = chrome.storage.local
 const storage_key = "subjects"
 
 function subjectsStoreInitializer() {
-	const { subscribe, update } = writable()
+    const { subscribe, update } = writable()
 
-	return {
-		subscribe,
-		set: (value) =>
-			update((data) => {
-				data = value
-				storage.set({ [storage_key]: value })
-				return data
-			}),
-		clear: () =>
-			update((data) => {
-				data = undefined
-				storage.remove(storage_key)
-			})
-	}
+    return {
+        subscribe,
+        set: (value) =>
+            update((data) => {
+                data = value
+                storage.set({ [storage_key]: data })
+                return data
+            }),
+        add_subject: (subject_name, msg_view) => {
+            update((data) => {
+                // check if key exists
+                let existing_ind = data.subjects.findIndex((s) => s.name === subject_name)
+                if (existing_ind !== -1) {
+                    data.subjects[existing_ind].msg_view.push(msg_view)
+                } else {
+                    data.subjects.push({
+                        key: nanoid(10),
+                        name: subject_name,
+                        msg_view: [msg_view],
+                    })
+                }
+
+                storage.set({ [storage_key]: data })
+                return data
+            })
+        },
+        clear: () =>
+            update((data) => {
+                data = undefined
+                storage.remove(storage_key)
+            })
+    }
 }
 
 export const subjectsStore = subjectsStoreInitializer()
 
 storage.get(storage_key, (result) => {
-	let data = result[storage_key]
-	if (data === undefined) {
-		console.log('dump initial config:', c_all_data)
-		subjectsStore.set(c_all_data)
-	} else {
-		subjectsStore.set(data)
-	}
+    let data = result[storage_key]
+    if (data === undefined) {
+        console.log('dump initial config:', c_all_data)
+        subjectsStore.set(c_all_data)
+    } else {
+        subjectsStore.set(data)
+    }
 })
