944063d8611b75b8b81411bcb0c2322f8a9212e8
Anelya Dusaeva <an.dusaeva+git@gmail.com>
2022-11-10 14:38:31 +0300

    resolving changes

--- ui/src/lib/components/pages/Popup.svelte
+++ ui/src/lib/components/pages/Popup.svelte
@@ -1,38 +1,30 @@
 <script context="module">
-    import Button from "src/lib/components/Button.svelte"
-    import Input from "src/lib/components/Input.svelte"
-    import Select from "src/lib/components/Select.svelte"
+  import Button from "src/lib/components/Button.svelte"
+  import Input from "src/lib/components/Input.svelte"
+  import SelectSubjects from "src/lib/components/SelectSubjects.svelte"
 
   import { requestDoc } from 'src/lib/api/fetchDoc.js'
-  // import { subjectsStore } from 'src/lib/store/subjects.js'
-  import { c_all_data } from "src/templates"
-  import logout from "~icons/mdi/logout?raw"
+
+  import { subjectsStore } from 'src/lib/store/subjects'
+
   import settings from "~icons/mdi/cog?raw"
 </script>
 
 <script>
-  let subjects = [];
-  for (let i = 0; i < c_all_data.subjects.length; i++) {
-    for (let j = 0; j < c_all_data.subjects[i].msg_view.length; j++)
-    subjects.push(c_all_data.subjects[i].msg_view[j])
-  }
-  console.log(subjects)
-  // let subjects = [
-  //   {
-  //     key: 'algo-krav',
-  //     name: 'Алгоритмы Кравченко',
-  //     email: 'krav@test.com',
-  //     subject: '[ИТМО] ДЗ 5 Фамилия Имя'
-  //   }
-  // ]
-  let selected_key = ''
-  let hw_number = 1
-  $: selected_subject = subjects.find(e => e.key === selected_key)
-  $: email = selected_subject?.email  
-  $: topic = selected_subject?.topic.replace('{num_hw}', hw_number)//format(topic, {num_hw: hw_number.toString})
+  $: subjects = $subjectsStore?.subjects
 
-    function requestDocCallback(i) {
-        console.log(`Called back! ${i}`);
+  let selected_subject_key = ''
+  let selected_msg_view_surname = ''
+
+  $: selected_subject = subjects?.find(e => e.key === selected_subject_key)
+  $: selected_view = selected_subject?.msg_view.find(v => v.surname === selected_msg_view_surname)
+
+  $: email = selected_view?.email
+  $: subject = selected_view?.topic
+  $: hw_number = selected_subject?.num_hw
+
+  function requestDocCallback (id) {
+    console.log('requestDocCallback with id', id)
 
         let content = JSON.stringify(
             {
@@ -41,8 +33,9 @@
                 "toAddress": "julie.meh@yandex.ru",
                 "text": "Отправляю домашнюю работу",
                 "subject": "Домашняя работа по алогсам",
-                "code": i,
-                "fileName": "Algos.pdf"
+                "code": id,
+                "fileName": "Algos.pdf",
+                "smtpService": "gmail",
             }
         );
 
@@ -56,20 +49,18 @@
             .then(res => res.json())
             .then(json => json.code)
             .catch(console.error)
-    }
+  }
 </script>
 
 <main class="p-3">
+  <navbar class="navbar is-flex is-justify-content-space-between mt-1">
     <h5 class="title is-5 mt-3">Посылатор</h5>
-    <navbar class="navbar is-flex is-justify-content-space-between mt-1">
-        <Button imageUrl={logout}/>
-        <Button imageUrl={settings} on:click={() => open('settings.html')}/>
-    </navbar>
+    <Button imageUrl={settings} on:click={() => open('settings.html')}/>
+  </navbar>
 
-  <Select options={subjects} id="subjects" title="Предмет" bind:value={selected_key} />
-  
+  <SelectSubjects subjects={subjects || []} bind:key={selected_subject_key} bind:surname={selected_msg_view_surname} />
   <Input id="email" label="Будет послано на" value={email} readonly />
-  <Input id="subject" label="С темой" value={topic} readonly />
+  <Input id="topic" label="С темой" value={subject} readonly />
   <Input id="subject" label="Номер дз" value={hw_number} type="number" />
-  <Button content="Послать" on:click={() => alert('sent')} />
+  <Button content="Послать" on:click={() => requestDoc(requestDocCallback)} class="mt-2" />
 </main>
