5985cf947c50bb7b3a1b166ff7bacb97bace47a6
Ilia <43654815+istudyatuni@users.noreply.github.com>
2022-11-10 20:17:43 +0300

    feat: add subject (#35)

--- ui/src/lib/components/Button.svelte
+++ ui/src/lib/components/Button.svelte
@@ -1,11 +1,11 @@
 <script>
     let className = ''
     export { className as class }
-    export let content = '', imageUrl = undefined
+    export let content = '', imageUrl = undefined, disabled = false
 </script>
 
 {#if imageUrl}
-    <button class="button {className}" on:click>{@html imageUrl}</button>
+    <button class="button {className}" on:click {disabled}>{@html imageUrl}</button>
 {:else}
-    <button class="button {className}" on:click>{content}</button>
+    <button class="button {className}" on:click {disabled}>{content}</button>
 {/if}
