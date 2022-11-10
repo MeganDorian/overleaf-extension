3d039a1bf028eb9d523aa211577ae9cebe7516e7
Julie Kosenko <mishakollins68@gmail.com>
2022-11-10 18:36:46 +0300

    refactor: added msgViewItem component for msgView

---
+++ ui/src/lib/components/MsgViewItem.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let label, value;
+</script>
+
+<main>
+    <div class="block">
+        <span class="label">{label}</span>
+        <p class="notification">{value}</p>
+    </div>
+</main>
